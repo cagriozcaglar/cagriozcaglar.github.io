3acec4b3c1b3659c4336a2bc1eaa09ccd80df3e5
Cagri Ozcaglar <cagriozcaglar@Mac.attlocal.net>
2025-09-06 10:09:05 -0700

    Update Projects tab

---
+++ commitAll.sh
@@ -0,0 +1,8 @@
+# git config --system core.editor emacs
+repo_name="cagriozcaglar.github.io"
+git add --all .
+git commit -m "$1"
+randomText="origin`cat /dev/urandom | tr -cd 'a-f0-9' | head -c 6`"
+git remote add ${randomText} https://github.com/cagriozcaglar/${repo_name}.git
+git remote -v
+git push ${randomText} master
